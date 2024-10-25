b4878f2d13165e93224e5d63854be67fb999098f
w1nn3t0u-o7 <mik.ziel7890@gmail.com>
2024-10-25 14:27:19 +0200

    lab3 done

--- Klasy/Circle.hpp
+++ Klasy/Circle.hpp
@@ -2,41 +2,41 @@
 
 class Circle {
 private:
-    double* radius;
-    Point center;
+    double* m_radius;
+    Point m_center;
 public:
-    Circle(double r, double centX, double centY): center(centX, centY) {
+    Circle(double r, double cent_x, double cent_y): m_center(cent_x, cent_y) {
         if (r < 0) {
             std::cout << "Radius cannot be a negative number!" << std::endl;
         }else {
-        radius = new double;
-        *radius = r;
+        m_radius = new double;
+        *m_radius = r;
         }
     }
     void setRadius(double r) {
         if (r < 0) {
             std::cout << "Radius cannot be a negative number!" << std::endl;
         }else {
-            *radius = r;
+            *m_radius = r;
         }
     }
     double getRadius() const {
-        return *radius;
+        return *m_radius;
     }
-    void setCenter(double centX, double centY) {
-        center.setX(centX);
-        center.setY(centY);
+    void setCenter(double cent_x, double cent_y) {
+        m_center.setX(cent_x);
+        m_center.setY(cent_y);
     }
     Point getCenter() const {
-        return center;
+        return m_center;
     }
-    double area() {
-        return 3.14*(*radius)*(*radius);
+    double area() const {
+        return 3.14*(*m_radius)*(*m_radius);
     }
-    void areaDisplay(void) {
+    void areaDisplay(void) const {
         std::cout << "Area of a circle is: " << area() << std::endl;
     }
     ~Circle() {
-        delete radius;
+        delete m_radius;
     }
 };
--- Klasy/Point.hpp
+++ Klasy/Point.hpp
@@ -2,21 +2,21 @@
 
 class Point {
 private:
-    double x {}, y {};
+    double m_x {}, m_y {};
 public:
-    Point(double x_temp, double y_temp): x(x_temp), y(y_temp) {
+    Point(double x_temp, double y_temp): m_x(x_temp), m_y(y_temp) {
     };
     void setX(double x_temp) {
-        x = x_temp;
+        m_x = x_temp;
     }
     double getX() const {
-        return x;
+        return m_x;
     }
     void setY(double y_temp) {
-        y = y_temp;
+        m_y = y_temp;
     }
     double getY() const {
-        return y;
+        return m_y;
     }
     void display(void) const {
         std::cout << "(" << getX() << ", " << getY() << ")" << std::endl;
